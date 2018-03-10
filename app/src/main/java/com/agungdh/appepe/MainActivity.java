11fd484bf3062fa003c59669c36d23fbaa0f6653
agungdh <agungdh@live.com>
2018-03-10 12:28:18 +0700

    comit

--- app/src/main/java/com/agungdh/appepe/MainActivity.java
+++ app/src/main/java/com/agungdh/appepe/MainActivity.java
@@ -1,35 +1,18 @@
 package com.agungdh.appepe;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ApiClient ApiClient = new ApiClient();
     MainActivity activity;
     String passwordHash;
     int login = 0;
@@ -60,8 +43,12 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         String alamat = "https://apps.agungdh.com/api_mahasiswa/";
 
-                        String respon = apiAmbilDataGet(alamat);
-                        Log.d("OKHTTP", "Respon = " + respon);
+                        try {
+                            String respon = ApiClient.apiAmbilDataGet(alamat);
+                            Log.d("OKHTTP", "Respon = " + respon);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }).start();
 
@@ -82,28 +69,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public String apiAmbilDataGet(String url) {
-        String respond = null;
-        Log.d("OKHTTP", "Melakukan panggilan GET");
 
-        OkHttpClient client = new OkHttpClient();
-        Log.d("OKHTTP", "client okhttp terbentuk");
-
-        Request permohonan = new Request.Builder()
-                .url(url)
-                .build();
-        Log.d("OKHTTP", "membuat permohonan selesai");
-
-        try {
-            Response respon = client.newCall(permohonan).execute();
-            Log.d("OKHTTP", "mengambil respon");
-            respond = respon.body().string();
-        } catch (Exception e) {
-            Log.d("OKHTTP", "ada error gan... dibwah");
-            e.printStackTrace();
-        }
-
-        return respond;
-    }
 
 }
