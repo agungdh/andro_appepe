61c75b2059ab0f7b3b566177c6a7c38672aa3ef8
agungdh <agungdh@live.com>
2018-03-10 16:12:07 +0700

    comit

--- app/src/main/java/com/agungdh/appepe/MainActivity.java
+++ app/src/main/java/com/agungdh/appepe/MainActivity.java
@@ -8,7 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.UnsupportedEncodingException;
+
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,19 +46,65 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        String alamat = "https://apps.agungdh.com/api_mahasiswa/";
-
                         try {
-                            String respon = ApiClient.apiAmbilDataGet(alamat);
+                            //hash
+                            String url= "http://10.42.0.1/api_appepe/login/hash";
+
+                            RequestBody body = new FormBody.Builder()
+                                    .add("password", password)
+                                    .build();
+                            Log.d("OKHTTP", "membuat form data");
+
+                            String respon = ApiClient.apiAmbilDataPost(url, body);
                             Log.d("OKHTTP", "Respon = " + respon);
+
+                            JSONObject json = new JSONObject(respon);
+                            String hashPassword = json.getString("hash");
+                            Log.d("OKHTTP", "Password ter hash = " + hashPassword);
+
+                            //login
+                            url = "http://10.42.0.1/api_appepe/login";
+
+                            body = new FormBody.Builder()
+                                    .add("username", username)
+                                    .add("password", hashPassword)
+                                    .build();
+                            Log.d("OKHTTP", "membuat form data");
+
+                            respon = ApiClient.apiAmbilDataPost(url, body);
+                            Log.d("OKHTTP", "Respon = " + respon);
+
+                            json = new JSONObject(respon);
+                            int jsonlogin = json.getInt("login");
+                            if (jsonlogin == 1) {
+                                String jsonid = json.getJSONObject("user").getString("id");
+                                String jsonusername = json.getJSONObject("user").getString("username");
+                                String jsonlevel = json.getJSONObject("user").getString("level");
+                                String jsonnama = json.getJSONObject("mahasiswa").getString("nama");
+                                String jsonnim = json.getJSONObject("mahasiswa").getString("nim");
+                                String jsonidmahasiswa = json.getJSONObject("mahasiswa").getString("id");
+
+                                String dataLogin = "ID User = " + jsonid + "\n"
+                                        + "Username = " + jsonusername + "\n"
+                                        + "Level = " + jsonlevel + "\n"
+                                        + "ID Mahasiswa = " + jsonidmahasiswa + "\n"
+                                        + "Nama = " + jsonnama + "\n"
+                                        + "NIM = " + jsonnim + "\n";
+
+
+                                Log.d("OKHTTP", "Data Login = " + dataLogin);
+                                login = 1;
+                                keMenuUtama();
+                            } else {
+                                Log.d("OKHTTP", "Login Salah");
+                            }
+
                         } catch (Exception e) {
+                            Log.d("OKHTTP", "Ada Error");
                             e.printStackTrace();
                         }
                     }
                 }).start();
-
-//                login = 1;
-//                keMenuUtama();
             }
         });
     }
